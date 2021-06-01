a8d465d3501823839d5cc978395bb8bd86f6b545
serghalak <serghalak@gmail.com>
2021-06-01 21:32:33 +0300

    multi-modual project

--- sandbox-web/src/main/java/com/exadel/sandbox/SandboxApplication.java
+++ sandbox-web/src/main/java/com/exadel/sandbox/SandboxApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SandboxApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(SandboxApplication.class, args);
 	}
 
