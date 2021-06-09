cc26e2dcc1ae87c9f25603ff892b0d49361dae7f
serghalak <serghalak@gmail.com>
2021-06-09 16:16:06 +0300

    add SecurityConfig  initialization

--- sandbox-web/src/main/java/com/exadel/sandbox/security/SecurityConfig.java
+++ sandbox-web/src/main/java/com/exadel/sandbox/security/SecurityConfig.java
@@ -1,4 +1,24 @@
 package com.exadel.sandbox.security;
 
-public class SecurityConfig {
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests(authorize -> {
+                    authorize.antMatchers("/",  "/login" ).permitAll();
+                } )
+                .authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin().and()
+                .httpBasic();
+    }
 }
