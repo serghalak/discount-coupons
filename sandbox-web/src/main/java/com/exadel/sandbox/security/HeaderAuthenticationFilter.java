d07a48db82a9405ab9af38efdbb30d508bd5e7da
serghalak <serghalak@gmail.com>
2021-06-10 11:43:54 +0300

    add success and unsuccess hadler in HeaderAuthenticationFilter

--- sandbox-web/src/main/java/com/exadel/sandbox/security/HeaderAuthenticationFilter.java
+++ sandbox-web/src/main/java/com/exadel/sandbox/security/HeaderAuthenticationFilter.java
@@ -1,13 +1,19 @@
 package com.exadel.sandbox.security;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.AbstractAuthenticationProcessingFilter;
 import org.springframework.security.web.util.matcher.RequestMatcher;
+import org.springframework.util.StringUtils;
 
+import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
@@ -17,6 +23,33 @@ public class HeaderAuthenticationFilter extends AbstractAuthenticationProcessing
 
     public HeaderAuthenticationFilter(RequestMatcher requiresAuthenticationRequestMatcher) {
         super(requiresAuthenticationRequestMatcher);
+    }
+
+
+    @Override
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain)
+            throws IOException, ServletException {
+
+        HttpServletRequest request = (HttpServletRequest) req;
+        HttpServletResponse response = (HttpServletResponse) res;
+
+
+        if (logger.isDebugEnabled()) {
+            logger.debug(">>>>>>Request is to process authentication");
+        }
+
+        try {
+            Authentication authResult = attemptAuthentication(request, response);
+
+            if (authResult != null) {
+                successfulAuthentication(request, response, chain, authResult);
+            } else {
+                chain.doFilter(request, response);
+            }
+        } catch (AuthenticationException e) {
+            log.error(">>>>>>>Authentication Failed", e);
+            unsuccessfulAuthentication(request, response, e);
+        }
     }
 
     @Override
@@ -33,8 +66,44 @@ public class HeaderAuthenticationFilter extends AbstractAuthenticationProcessing
         log.debug(">>>>>HeaderAuthenticationFilte User: " + userName);
         UsernamePasswordAuthenticationToken token =
                 new UsernamePasswordAuthenticationToken(userName, password);
-        return getAuthenticationManager().authenticate(token);
+        if(!userName.isEmpty()){
+            return this.getAuthenticationManager().authenticate(token);
+        }else{
+            return null;
+        }
+
     }
+
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request,
+                                            HttpServletResponse response, FilterChain chain, Authentication authResult)
+            throws IOException, ServletException {
+
+        if (logger.isDebugEnabled()) {
+            logger.debug("Authentication success. Updating SecurityContextHolder to contain: "
+                    + authResult);
+        }
+
+        SecurityContextHolder.getContext().setAuthentication(authResult);
+
+    }
+
+    @Override
+    protected void unsuccessfulAuthentication(HttpServletRequest request,
+                                              HttpServletResponse response, AuthenticationException failed)
+            throws IOException, ServletException {
+
+        SecurityContextHolder.clearContext();
+
+        if (log.isDebugEnabled()) {
+            log.debug(">>>>>Clear SecurityContext... Authentication request failed: "
+                    + failed.toString(),failed);
+        }
+
+        response.sendError(HttpStatus.UNAUTHORIZED.value(),
+                HttpStatus.UNAUTHORIZED.getReasonPhrase());
+    }
+
 
     private String getPassword(HttpServletRequest request) {
 
