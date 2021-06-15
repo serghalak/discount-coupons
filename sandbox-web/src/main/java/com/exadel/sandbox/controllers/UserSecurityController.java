54cb96eb7175d6ec52d34fe9feaef27c24126993
RosKokos <rostik143@gmail.com>
2021-06-15 10:46:53 +0300

    Add imports UserDto

--- sandbox-web/src/main/java/com/exadel/sandbox/controllers/UserSecurityController.java
+++ sandbox-web/src/main/java/com/exadel/sandbox/controllers/UserSecurityController.java
@@ -1,26 +1,17 @@
 package com.exadel.sandbox.controllers;
 
-import com.exadel.sandbox.dto.request.UserDto;
 import com.exadel.sandbox.security.utill.JwtUtil;
 import com.exadel.sandbox.service.impl.UserSecurityServiceImpl;
 import com.exadel.sandbox.ui.request.UserRequest;
 import com.exadel.sandbox.ui.response.AuthenticationResponse;
-import com.exadel.sandbox.ui.response.UserResponse;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 
