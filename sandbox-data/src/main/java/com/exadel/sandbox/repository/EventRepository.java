3d8c207e620ffaf7e37e9bf5f15ef80e5759d6f1
TarasKondratiuk <taraskondratjuk@.gmail.com>
2021-06-16 09:59:18 +0300

    add-EventDto, edit endpoint getAllEventsByUserLocation, edit DockerConfig

--- sandbox-data/src/main/java/com/exadel/sandbox/repository/EventRepository.java
+++ sandbox-data/src/main/java/com/exadel/sandbox/repository/EventRepository.java
@@ -1,9 +1,14 @@
 package com.exadel.sandbox.repository;
 
+import com.exadel.sandbox.model.location.Location;
 import com.exadel.sandbox.model.vendorinfo.Event;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public interface EventRepository extends JpaRepository<Event,Integer> {
+public interface EventRepository extends JpaRepository<Event, Integer> {
+
+    List<Event> findEventByLocations(Location location);
 }
--- sandbox-service/src/main/java/com.exadel.sandbox/service/impl/EventServiceImp.java
+++ sandbox-service/src/main/java/com.exadel.sandbox/service/impl/EventServiceImp.java
@@ -7,7 +7,6 @@ import com.exadel.sandbox.repository.EventRepository;
 import com.exadel.sandbox.repository.UserRepository;
 import com.exadel.sandbox.service.EventService;
 import lombok.AllArgsConstructor;
-import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.security.Principal;
@@ -24,26 +23,27 @@ public class EventServiceImp implements EventService {
     @Override
     public List<EventDto> getAllEventsByUserLocation(Principal principal) {
         User user = userRepository.findByEmail(principal.getName());
-        List<Event> events = eventRepository.findAll(Sort.by("dateEnd"));
-        return events.stream().filter(event->event.getLocations().stream().iterator().next().getCity()==user.getLocation().getCity())
-                .map(event -> EventDto.builder()
-                .id(event.getId())
-                .name(event.getName())
-                .shortDescription(event.getDescription())
-                .vendor(event.getProducts().stream().iterator().next().getVendor().getName())
-                .price(event.getPrice())
-                .discount(event.getDiscount())
-                .country(event.getLocations().stream().iterator().next().getCity().getCountry().getName())
-                .city(event.getLocations().stream().iterator().next().getCity().getName())
-                .status(event.getStatus().name())
-                .online(String.valueOf(event.isOnline()))
-                .location(event.getLocations().iterator().next().getCity().getLocations())
-                .dateBegin(event.getDateBegin())
-                .dateEnd(event.getDateEnd())
-                .quantityOfCoupons(event.getTotalCount())
-                .maxQuantityPerCustomer(event.getLimitation())
-                .detailedDescription(event.getProducts().stream().iterator().next().getDescription())
-                .build()).collect(Collectors.toList());
-    }
+        List<Event> events = eventRepository.findEventByLocations(user.getLocation());
 
+        return events.stream()
+                .sorted((event1, event2) -> event1.getDateEnd().compareTo(event2.getDateEnd()))
+                .map(event -> EventDto.builder()
+                        .id(event.getId())
+                        .name(event.getName())
+                        .shortDescription(event.getDescription())
+                        .vendor(event.getProducts().stream().iterator().next().getVendor().getName())
+                        .price(event.getPrice())
+                        .discount(event.getDiscount())
+                        .country(event.getLocations().stream().iterator().next().getCity().getCountry().getName())
+                        .city(event.getLocations().stream().iterator().next().getCity().getName())
+                        .status(event.getStatus().name())
+                        .online(String.valueOf(event.isOnline()))
+                        .location(event.getLocations().iterator().next().getCity().getLocations())
+                        .dateBegin(event.getDateBegin())
+                        .dateEnd(event.getDateEnd())
+                        .quantityOfCoupons(event.getTotalCount())
+                        .maxQuantityPerCustomer(event.getLimitation())
+                        .detailedDescription(event.getProducts().stream().iterator().next().getDescription())
+                        .build()).collect(Collectors.toList());
+    }
 }
