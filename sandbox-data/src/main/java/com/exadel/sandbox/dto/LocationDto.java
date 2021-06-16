e9b3440d8f2775062010233ac9c9510b3d98482f
KrasuliaV <Fenix2104>
2021-06-16 22:34:23 +0300

    Fixed CityRepository

--- sandbox-data/src/main/java/com/exadel/sandbox/dto/LocationDto.java
+++ sandbox-data/src/main/java/com/exadel/sandbox/dto/LocationDto.java
@@ -1,7 +1,6 @@
 package com.exadel.sandbox.dto;
 
 import com.exadel.sandbox.model.BaseEntity;
-import com.exadel.sandbox.model.location.City;
 import lombok.*;
 import org.springframework.stereotype.Component;
 
@@ -21,5 +20,5 @@ public class LocationDto extends BaseEntity {
 
     private String number;
 
-    private City city;
+    private CityDto city;
 }
--- sandbox-data/src/main/java/com/exadel/sandbox/repository/location_repository/CityRepository.java
+++ sandbox-data/src/main/java/com/exadel/sandbox/repository/location_repository/CityRepository.java
@@ -4,6 +4,7 @@ import com.exadel.sandbox.model.location.City;
 import com.exadel.sandbox.model.vendorinfo.Status;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -19,12 +20,16 @@ public interface CityRepository extends JpaRepository<City, Long> {
             "WHERE e.status = ?1")
     Set<City> findCitiesByEventStatus(Status status);
 
+    //    @Query("select l.city from Location l " +
+//            "where l.id in (SELECT sav_ev.locations from User u " +
+//            "join u.savedEvents sav_ev " +
+//            "WHERE u.id = ?1)")
     @Query(value = "SELECT DISTINCT city.* " +
             "FROM city LEFT JOIN location l ON city.id = l.city_id " +
             "LEFT JOIN event_location el ON l.id = el.location_id " +
             "LEFT JOIN event e ON el.event_id = e.id " +
             "LEFT JOIN saved_event se ON e.id = se.event_id " +
-            "WHERE se.user_id = ?2;", nativeQuery = true)
-    Set<City> findCitiesByFavoriteEvents(Long id);
+            "WHERE se.user_id = :userId", nativeQuery = true)
+    Set<City> findCitiesByFavoriteEvents(@Param("userId") Long id);
 
 }
