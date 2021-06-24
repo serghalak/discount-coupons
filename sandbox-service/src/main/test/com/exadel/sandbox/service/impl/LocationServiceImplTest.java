281935bdab8a09d988764ed7e164d992844d03ad
RosKokos <rostik143@gmail.com>
2021-06-24 12:50:48 +0300

    Bug fixed

---
+++ sandbox-service/src/main/test/com/exadel/sandbox/service/impl/LocationServiceImplTest.java
@@ -0,0 +1,32 @@
+package com.exadel.sandbox.service.impl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocationServiceImplTest {
+
+    @Autowired
+    private LocationServiceImpl locationService;
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void findByCity() {
+    }
+
+    @Test
+    void getAllLocationByCity() {
+    }
+
+    @Test
+    void create() {
+    }
+
+    @Test
+    void update() {
+    }
+}
