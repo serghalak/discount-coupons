18b7619036cd87b2aa7ea317b9f816da8af1c714
KrasuliaV <Fenix2104>
2021-06-24 23:25:09 +0300

    Added sending message

---
+++ sandbox-service/src/main/java/com.exadel.sandbox/mail/JavaMailSenderConfig.java
@@ -0,0 +1,27 @@
+package com.exadel.sandbox.mail;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+@Configuration
+public class JavaMailSenderConfig {
+    @Bean
+    public JavaMailSender getJavaMailSender() {
+        var mailSender = new JavaMailSenderImpl();
+        mailSender.setHost("smtp.gmail.com");
+        mailSender.setPort(587);
+
+        mailSender.setUsername("exadelteam2021@gmail.com");
+        mailSender.setPassword("vjjufiujxgvednuc");
+
+        var props = mailSender.getJavaMailProperties();
+        props.put("mail.transport.protocol", "smtp");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.ssl.trust", "smtp.gmail.com");
+
+        return mailSender;
+    }
+}
