0f9828a2c89bbb498591cfc9e64f3a0027ed3783
Helen1711 <helen.mysiak@gmail.com>
2021-06-30 09:43:45 +0300

    Changed endpoint findAll vendors.

--- sandbox-service/src/main/java/com.exadel.sandbox/service/TagService.java
+++ sandbox-service/src/main/java/com.exadel.sandbox/service/TagService.java
@@ -3,8 +3,6 @@ package com.exadel.sandbox.service;
 import com.exadel.sandbox.dto.pagelist.PageList;
 import com.exadel.sandbox.dto.response.tag.TagResponse;
 
-import java.util.Set;
-
 public interface TagService {
 
     PageList<TagResponse> findAll(Integer pageNumber, Integer pageSize);
