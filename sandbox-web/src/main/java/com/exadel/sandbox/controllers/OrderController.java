f4ee5a7b2ba8571de158c963b0e90e7d76ef0720
RosKokos <rostik143@gmail.com>
2021-07-05 21:19:16 +0300

    GetAllFromOrder done

--- sandbox-web/src/main/java/com/exadel/sandbox/controllers/OrderController.java
+++ sandbox-web/src/main/java/com/exadel/sandbox/controllers/OrderController.java
@@ -20,7 +20,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class OrderController {
 
     private final OrderService orderService;
-    private final EventRepository eventRepository;
     private final JwtUtil jwtUtil;
 
     @PostMapping(path = "/addEvent/toOrder")
